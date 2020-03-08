98d6b9e173cfefaf694510598d8167bf2a8decda
chj4535 <chj4535@ajou.ac.kr>
2020-03-08 15:27:57 +0900

    start

---
+++ app/src/main/java/choi/mvvm/androidchatting/MainActivity.kt
@@ -0,0 +1,13 @@
+package choi.mvvm.androidchatting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import choi.mvvm.androidchatting.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
