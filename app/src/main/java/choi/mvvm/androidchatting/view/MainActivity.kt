6ebcd038148c4a2145f4f66af7ba4a2bfb8983d9
chj4535 <chj4535@ajou.ac.kr>
2020-03-08 17:56:25 +0900

    fragment a,b,c configure

--- app/src/main/java/choi/mvvm/androidchatting/view/MainActivity.kt
+++ app/src/main/java/choi/mvvm/androidchatting/view/MainActivity.kt
@@ -2,12 +2,43 @@ package choi.mvvm.androidchatting.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
 import choi.mvvm.androidchatting.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,BottomNavigationView.OnNavigationItemSelectedListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<View>(R.id.bottomnavigationview) as BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragementView,FriendListFragment())
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Log.d("onNavigationItemSelected",item.itemId.toString())
+        when(item.itemId){
+            R.id.navigation_friendList->{
+                val fragmentA = FriendListFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragementView,fragmentA).commit()
+            }
+            R.id.navigation_chatList->{
+                val fragmentB = ChatListFragement()
+                supportFragmentManager.beginTransaction().replace(R.id.fragementView,fragmentB).commit()
+            }
+            R.id.navigation_info->{
+                val fragmentC = InfoFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragementView,fragmentC).commit()
+            }
+
+        }
+        return true
     }
 }
